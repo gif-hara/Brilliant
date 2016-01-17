e9297f748459428020b9bd5f31876853fd0043ea
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2016-01-18 01:37:56 +0900

    プロジェクト追加

---
+++ Assets/Scripts/PullTest.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.EventSystems;
+using System.Collections.Generic;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+namespace Brilliant
+{
+	/// <summary>
+	/// .
+	/// </summary>
+	public class PullTest : MonoBehaviour
+	{
+		public void OnPull()
+		{
+			var horizontal = CrossPlatformInputManager.GetAxis("Horizontal");
+			var vertical = CrossPlatformInputManager.GetAxis("Vertical");
+			Debug.Log("horizontal = " + horizontal + " vertical = " + vertical);
+		}
+	}
+}
