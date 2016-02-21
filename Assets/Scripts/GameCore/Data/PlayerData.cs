e4ecaa6531be3a9e58499ce48e49cdeb06edd5f2
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2016-02-22 00:38:40 +0900

    リストを作成中

--- Assets/Scripts/GameCore/Data/PlayerData.cs
+++ Assets/Scripts/GameCore/Data/PlayerData.cs
@@ -12,11 +12,14 @@ namespace Brilliant
 	{
 		public List<Item> Items{ private set; get; }
 
+		public List<Weapon> Weapons{ private set; get; }
+
 		public Wallet Wallet{ private set; get; }
 
 		public PlayerData()
 		{
 			this.Items = new List<Item>();
+			this.Weapons = new List<Weapon>();
 			this.Wallet = new Wallet();
 		}
 	}
