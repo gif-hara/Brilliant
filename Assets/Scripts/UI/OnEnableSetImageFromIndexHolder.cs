9b18ab258abbafb4d8932a37c5e662c8e473ec29
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2016-02-21 17:00:02 +0900

    初期化のタイミングでSpriteを設定出来るよう実装

---
+++ Assets/Scripts/UI/OnEnableSetImageFromIndexHolder.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.EventSystems;
+using System.Collections.Generic;
+
+namespace Brilliant
+{
+	/// <summary>
+	/// OnEnableのタイミングでIndexHolderからImageを設定するコンポーネント.
+	/// </summary>
+	public class OnEnableSetImageFromIndexHolder : MonoBehaviour
+	{
+		[SerializeField]
+		private IndexHolder indexHolder;
+
+		[SerializeField]
+		private ModifiedIndexSetImage imageSetter;
+
+		void OnEnable()
+		{
+			this.imageSetter.OnModifiedIndex(this.indexHolder.Index);
+		}
+	}
+}
